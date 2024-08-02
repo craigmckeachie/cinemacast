f3e7026523ba0982b463c89a39ff6d10bbec0b32
Craig McKeachie <craigdmckeachie@gmail.com>
2024-08-01 20:02:52 -0400

    busy working

--- cinemacast-client/src/movies/MovieList.tsx
+++ cinemacast-client/src/movies/MovieList.tsx
@@ -4,10 +4,13 @@ import { Movie } from "./Movie";
 
 function MovieList() {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [busy, setBusy] = useState(false);
 
   async function loadMovies() {
+    setBusy(true);
     const data = await movieAPI.list();
     setMovies(data);
+    setBusy(false);
   }
 
   useEffect(() => {
@@ -15,19 +18,28 @@ function MovieList() {
   }, []);
 
   return (
-    <section className="list">
-      {movies.map((movie) => (
-        <article className="card">
-          <strong>{movie.title}</strong>
-          <small>
-            Genre: {movie.genre} ({movie.year})
-          </small>
-          <small></small>
-          <small>Rating: {movie.rating}</small>
-          <small>Director: {movie.director}</small>
-        </article>
-      ))}
-    </section>
+    <>
+      {busy && (
+        <section className="busy-container">
+          <button aria-busy="true" className="outline secondary">
+            Please wait…
+          </button>
+        </section>
+      )}
+      <section className="list">
+        {movies.map((movie) => (
+          <article className="card">
+            <strong>{movie.title}</strong>
+            <small>
+              Genre: {movie.genre} ({movie.year})
+            </small>
+            <small></small>
+            <small>Rating: {movie.rating}</small>
+            <small>Director: {movie.director}</small>
+          </article>
+        ))}
+      </section>
+    </>
   );
 }
 
